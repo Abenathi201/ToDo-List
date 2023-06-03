72a2e46f37b60f074caf443847a0518e2e9c0ef6
Abenathi201 <abenathi.sindaphi201@gmail.com>
2023-06-03 15:00:56 +0200

    almost there

--- script/script.js
+++ script/script.js
@@ -1,24 +1,102 @@
-let input = document.getElementById("input");
+let addedInput = document.getElementById("input");
 let display = document.getElementById("display");
 let button = document.getElementById("submit");
+let sort = document.getElementById("sort")
+ 
 
-button.addEventListener("click", function(){
-    let text = document.createElement("p");
-    text.innerHTML = input.value;
-    display.appendChild(text);
-    storeData();
+let myArray = [];
 
-    text.addEventListener("click", function() {
-        text.style.textDecoration = "line-through";
-        storeData();
+button.addEventListener("click", function(event){
+    event.preventDefault();
+    let todo = localStorage.getItem("todo");
+    if(todo == null){
+        // myArray = [];
+        alert("Enter your to do list")
+    } else  {
+        myArray = JSON.parse(todo);
+    }
+
+    myArray.push(addedInput.value)
+    addedInput.value = "";
+    localStorage.setItem("todo", JSON.stringify(myArray))
+    displayTodo()
+    
+
+
+
+    // let text = document.createElement("p");
+    // text.innerHTML = input.value;
+    // display.appendChild(text);
+
+    // text.addEventListener("click", function() {
+    //     text.style.textDecoration = "line-through";
     })
-    function storeData() {
-        localStorage.setItem("data", text.innerHTML)
+
+    function displayTodo() {
+        let todo = localStorage.getItem("todo");
+        if(todo == null) {
+            // myArray = [];
+            alert("Enter your to do list")
+        } else {
+            myArray = JSON.parse(todo);
+        }
+
+        display.innerHTML = "";
+        myArray.forEach((data, index) => {
+            // container += `
+            
+            // `
+            display.innerHTML += `
+                <p>${data}</p>
+                <button onclick="deleteItems(${index})" class="fa-sharp fa-solid fa-trash""></button>
+                <button onclick="editItems(${index})" class="fa-solid fa-pen"></button>
+            `
+            display.addEventListener("click", function() {
+                display.style.textDecoration = "line-through";
+            })
+        });
+        
+}
+
+    function deleteItems(index) {
+        let todo = localStorage.getItem("todo");
+        myArray = JSON.parse(todo);
+        myArray.pop(index, 1);
+        localStorage.setItem("todo", JSON.stringify(myArray));
+        displayTodo();
     }
-    function showData() {
-        text.innerHTML = localStorage.getItem("data")
-    }
-    showData()
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const dom = localStorage.getItem("todo");
+        if(dom) {
+            todo = JSON.parse(dom);
+            todo.forEach((d) => {
+                displayTodo(d)
+            });
+        }
+    })
+
+    sort.addEventListener("click", function () {
+
+        myArray.sort((a, b) => {
+            if(a.value > b.value) {
+                return 1;
+            } else if (a.value < b.value) {
+                return -1;
+            }
+        })
+    })
+
+
+
+
+    // function storeData() {jna
+    //     localStorage.setItem("data", text.innerHTML)
+    // }
+    // function showData() {
+    //     text.innerHTML = localStorage.getItem("data")
+    // }
+    // showData()
 
     // function getValues() {
     //     let storedData = window.localStorage.items;
@@ -32,4 +110,4 @@ button.addEventListener("click", function(){
     //     }
     // }
     // getValues();
-})
+//})
