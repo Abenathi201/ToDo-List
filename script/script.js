fc976f6379e997f7f6faca5b701c9b44b619cce4
Abenathi201 <abenathi.sindaphi201@gmail.com>
2023-07-18 15:48:01 +0200

    added a sort button

--- script/script.js
+++ script/script.js
@@ -1,127 +1,81 @@
-let addedInput = document.getElementById("input");
-let display = document.getElementById("display");
-let button = document.getElementById("submit");
-let sort = document.getElementById("sort")
- 
+// Select elements
+const addedInput = document.getElementById("input");
+const display = document.getElementById("display");
+const button = document.getElementById("submit");
+const sort = document.getElementById("sort");
 
 let myArray = [];
 
-button.addEventListener("click", function(event){
-    event.preventDefault();
-    let todo = localStorage.getItem("todo");
-    if(todo == null){
-        // myArray = [];
-        alert("Enter your to do list")
-    } else  {
-        myArray = JSON.parse(todo);
-    }
+button.addEventListener("click", function(event) {
+  event.preventDefault();
 
-    myArray.push(addedInput.value)
-    addedInput.value = "";
-    localStorage.setItem("todo", JSON.stringify(myArray))
-    displayTodo()
-    
+  // Validate input
+  if (addedInput.value.trim() === "") {
+    alert("Please enter a task");
+    return;
+  }
 
+  let todo = localStorage.getItem("todo");
+  if (todo == null) {
+    myArray = [];
+  } else {
+    myArray = JSON.parse(todo);
+  }
 
+  // Check for duplicate task
+  if (myArray.includes(addedInput.value)) {
+    alert("Task already exists");
+    return;
+  }
 
-    // let text = document.createElement("p");
-    // text.innerHTML = input.value;
-    // display.appendChild(text);
+  myArray.push(addedInput.value);
+  addedInput.value = "";
+  localStorage.setItem("todo", JSON.stringify(myArray));
+  displayTodo();
+});
 
-    // text.addEventListener("click", function() {
-    //     text.style.textDecoration = "line-through";
+function displayTodo() {
+  let todo = localStorage.getItem("todo");
+  if (todo == null) {
+    myArray = [];
+    alert("Enter your to-do list");
+  } else {
+    myArray = JSON.parse(todo);
+  }
+
+  display.innerHTML = "";
+  myArray.forEach((task, index) => {
+    const taskElement = document.createElement("div");
+    taskElement.innerHTML = `
+      <p>${task}</p>
+      <button onclick="deleteTask(${index})" class="fa-sharp fa-solid fa-trash"></button>
+    `;
+    display.appendChild(taskElement);
+
+    taskElement.addEventListener("click", function() {
+        taskElement.style.textDecoration = "line-through";
     })
-
-    function displayTodo() {
-        let todo = localStorage.getItem("todo");
-        if(todo == null) {
-            // myArray = [];
-            alert("Enter your to do list")
-        } else {
-            myArray = JSON.parse(todo);
-        }
-
-        display.innerHTML = "";
-        myArray.forEach((data, index) => {
-            // container += `
-            
-            // Have to use getElemets by something
-            display.innerHTML += `
-                <p id="text">${data}</p>
-                <button onclick="deleteItems(${index})" class="fa-sharp fa-solid fa-trash""></button>
-            `
-            p.addEventListener("click", function() {
-                p.style.textDecoration = "line-through";
-            })
-        });
-        
+  });
 }
 
-    function deleteItems(index) {
-        let todo = localStorage.getItem("todo");
-        myArray = JSON.parse(todo);
-        myArray.pop(index, 1);
-        localStorage.setItem("todo", JSON.stringify(myArray));
-        displayTodo();
-    }
+function deleteTask(index) {
+  let todo = localStorage.getItem("todo");
+  myArray = JSON.parse(todo);
+  myArray.splice(index, 1);
+  localStorage.setItem("todo", JSON.stringify(myArray));
+  displayTodo();
+}
 
-    document.addEventListener('DOMContentLoaded', () => {
-        let dom = localStorage.getItem("todo");
-        if(dom) {
-            todo = JSON.parse(dom);
-            todo.forEach((d) => {
-                displayTodo(d)
-            });
-        }
-    })
+document.addEventListener("DOMContentLoaded", () => {
+  displayTodo();
+});
 
-    sort.addEventListener("click", function () {
+sort.addEventListener("click", function() {
+  let todo = localStorage.getItem("todo");
+  myArray = JSON.parse(todo);
 
-        // myArray.sort((a, b) => {
-        //     if(a.value > b.value) {
-        //         return 1;
-        //     } else if (a.value < b.value) {
-        //         return -1;
-        //     }
-        // })
+  myArray.sort((a, b) => a.localeCompare(b));
 
-        // let sortArray = localStorage.getItem("todo");
-        // if(sortArray) {
-        //     todo = JSON.parse(sortArray);
-        //     todo.sort((a, b) => {
-        //         if (a.value > b.value) {
-        //             return 1;
-        //         }
-        //         else if (a.value < b.value) {
-        //             return -1;
-        //         }
-        //     })
-        // }
-        
-
-    })
-
-
-
-
-    // function storeData() {jna
-    //     localStorage.setItem("data", text.innerHTML)
-    // }
-    // function showData() {
-    //     text.innerHTML = localStorage.getItem("data")
-    // }
-    // showData()
-
-    // function getValues() {
-    //     let storedData = window.localStorage.items;
-    //     if(!storedData) {
-    //         text.innerHTML = "<p>Make a to list</p>"+
-    //                          "<p>Make sure to complete your to do list</p>"+
-    //                          "<p>After completion make sure to reward yourself</p>"
-    //     }
-    //     else {
-    //         text.innerHTML = storedData
-    //     }
-    // }
-    // getValues();
-//})
+  localStorage.setItem("todo", JSON.stringify(myArray));
+  displayTodo();
+});
